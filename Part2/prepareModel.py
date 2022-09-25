d47f9ae69ec3b2e795ffde89ba56b28e512d13af
Netipoom <88016874+Netipoom@users.noreply.github.com>
2022-09-25 15:07:50 +0700

    Add files via upload

--- Part2/prepareModel.py
+++ Part2/prepareModel.py
@@ -5,13 +5,7 @@ from collections import OrderedDict
 def prepareModel(arch,hidden_unit):
     if arch == 'VGG':
         model = models.vgg16(pretrained=True)
-    elif arch == 'Densenet':
-        model = models.densenet121(pretrained=True)
-    else:
-        model = getattr(models,arch)(pretrained = True)
-    for param in model.parameters():
-        param.requires_grad = False
-    classifier = nn.Sequential(OrderedDict([
+        classifier = nn.Sequential(OrderedDict([
                             ('fc1', nn.Linear(25088, hidden_unit)),
                             ('relu1', nn.ReLU()),
                             ('dropout1',nn.Dropout(p=0.2)),
@@ -24,6 +18,23 @@ def prepareModel(arch,hidden_unit):
                             ('fc4', nn.Linear(255, 102)),
                             ('output', nn.LogSoftmax(dim=1))
                             ]))
+    elif arch == 'Densenet':
+        model = models.densenet121(pretrained=True)
+        classifier = nn.Sequential(OrderedDict([
+                            ('fc1', nn.Linear(1024, hidden_unit)),
+                            ('relu1', nn.ReLU()),
+                            ('dropout1',nn.Dropout(p=0.2)),
+                            ('fc2', nn.Linear(hidden_unit, 512)),
+                            ('relu2', nn.ReLU()),
+                            ('dropout2',nn.Dropout(p=0.4)),
+                            ('fc3', nn.Linear(512, 255)),
+                            ('relu3', nn.ReLU()),
+                            ('dropout3',nn.Dropout(p=0.3)),
+                            ('fc4', nn.Linear(255, 102)),
+                            ('output', nn.LogSoftmax(dim=1))
+                            ]))
+    for param in model.parameters():
+        param.requires_grad = False
         
     model.classifier = classifier
     criterion = nn.NLLLoss()
